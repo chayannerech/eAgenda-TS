8dbdd44b8e89d98245468eb3e3adec8b070ffdbb
Chayanne Rech <rechchayanne@gmail.com>
2024-10-17 02:41:50 -0300

    Estruturado o projeto. Criadas as pastas shell e dashboard.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShellComponent } from "./core/shell/shell.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ShellComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'eAgenda';

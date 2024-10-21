8ebf803939cb2723a74364f77616f01601cf58ca
Chayanne Rech <rechchayanne@gmail.com>
2024-10-21 15:19:47 -0300

    Ajustada a rota, em caso de logout

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,31 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, Router, Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { RegistroComponent } from './core/auth/views/registro/registro.component';
 import { LoginComponent } from './core/auth/views/login/login.component';
 import { categoriasRoutes } from './views/categorias/categorias.routes';
+import { UsuarioService } from './core/auth/service/usuario.service';
+import { inject } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+const authGuard: CanMatchFn = (): Observable<boolean> => {
+  const router = inject(Router);
+  const usuarioService = inject(UsuarioService)
+
+  return usuarioService.usuarioAutenticado
+    .pipe(map(usuario => {
+      if (!usuario) {
+        router.navigate(['/login'])
+        return false;
+      }
+      return true;
+    }));
+}
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'dashboard', component: DashboardComponent, canMatch:[authGuard]},
   { path: 'categorias', children: categoriasRoutes },
 ];

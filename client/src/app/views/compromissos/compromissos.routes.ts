f3d0c890a0b8f6c50ef8880230018366847c1ca7
Chayanne Rech <rechchayanne@gmail.com>
2024-10-22 17:48:24 -0300

    Feita a listagem de compromissos

--- client/src/app/views/compromissos/compromissos.routes.ts
+++ client/src/app/views/compromissos/compromissos.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from "@angular/router";
+import { ListarCompromissosComponent } from "./listar/listar-compromissos.component";
 
 export const compromissosRoutes: Routes = [
   { path: '', redirectTo: 'listar', pathMatch: 'full'},
-  // { path: 'listar', component: ListarContatosComponent},
+  { path: 'listar', component: ListarCompromissosComponent},
   // { path: 'inserir', component: InserirContatoComponent},
   // { path: 'editar/:id', component: EditarContatoComponent},
   // { path: 'excluir/:id', component: ExcluirContatoComponent}

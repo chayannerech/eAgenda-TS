8c5f767bae57b6901632e9537b0e15ab93b592c3
Chayanne Rech <rechchayanne@gmail.com>
2024-10-23 17:22:56 -0300

    Ajustada a listagem e criados os componentes de edição de compromissos

---
+++ client/src/app/views/compromissos/editar/editar-compromissos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-compromissos',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-compromissos.component.html',
+  styleUrl: './editar-compromissos.component.scss'
+})
+export class EditarCompromissosComponent {
+
+}

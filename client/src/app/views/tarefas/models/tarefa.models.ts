7c9f4eb8465aa86661be4157e69d0f76e4e4b878
Chayanne Rech <rechchayanne@gmail.com>
2024-10-26 17:47:44 -0300

    Ajustados os models de tarefas

--- client/src/app/views/tarefas/models/tarefa.models.ts
+++ client/src/app/views/tarefas/models/tarefa.models.ts
@@ -1,48 +1,52 @@
 export interface ListarTarefasViewModel {
   id: string;
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  dataCriacao: string;
+  prioridade: string;
+  situacao: string;
 }
 export interface InserirTarefaViewModel {
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  prioridade: number;
+  itens: ItensTarefaViewModel;
 }
 export interface TarefaInseridaViewModel {
   id: string;
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  prioridade: number;
+  itens: ItensTarefaViewModel;
 }
 export interface EditarTarefaViewModel {
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  prioridade: number;
+  itens: ItensTarefaViewModel;
 }
 export interface TarefaEditadaViewModel {
   id: string;
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  prioridade: number;
+  itens: ItensTarefaViewModel;
+}
+export interface ItensTarefaViewModel {
+  id: string;
+  titulo: string;
+  status: number;
+  concluido: boolean;
 }
 export interface DetalhesTarefaViewModel {
   id: string;
-  nome: string;
-  email: string;
-  empresa: string;
-  cargo: string;
-  telefone: string;
+  titulo: string;
+  dataCriacao: string;
+  dataConclusao: string;
+  quantidadedeItens: number;
+  percentualConcluido: number;
+  prioridade: string;
+  situacao: string;
+  itens: ItensTarefaDetalhesViewModel
+}
+export interface ItensTarefaDetalhesViewModel {
+  titulo: string;
+  situacao: string;
 }
 
 export interface TarefaExcluidaViewModel {}

912bc0e521477625eea2820d87a73cc858a234b0
Chayanne Rech <rechchayanne@gmail.com>
2024-10-25 23:47:53 -0300

    Mudei para a API atualizada

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -4,7 +4,7 @@ import { environment } from './src/environments/environment.development'
 export default defineConfig({
 
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    'baseUrl': 'http://localhost:4400',
     supportFile: './cypress/support/e2e.ts',
     env: {
       apiUrl: environment.apiUrl,

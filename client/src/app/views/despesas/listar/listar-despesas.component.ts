280a69faeb4941816f04020033d5f9bfce09129e
Chayanne Rech <rechchayanne@gmail.com>
2024-10-31 14:28:23 -0300

    Feita a listagem de despesas

--- client/src/app/views/despesas/listar/listar-despesas.component.ts
+++ client/src/app/views/despesas/listar/listar-despesas.component.ts
@@ -5,13 +5,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { ListarCompromissosViewModel } from '../models/compromisso.models';
+import { ListarDespesasViewModel } from '../models/despesa.models';
 import { BotaoCadastrarComponent } from "../../partials/botao-cadastrar/botao-cadastrar.component";
 import { TituloComponent } from "../../partials/titulo/titulo.component";
 import { CardFooterComponent } from "../../partials/card-footer/card-footer.component";
 
 @Component({
-  selector: 'app-listar-compromissos',
+  selector: 'app-listar-despesas',
   standalone: true,
   imports: [
     RouterLink,
@@ -26,16 +26,16 @@ import { CardFooterComponent } from "../../partials/card-footer/card-footer.comp
     TituloComponent,
     CardFooterComponent
   ],
-  templateUrl: './listar-compromissos.component.html',
-  styleUrl: '../styles/compromissos.scss'
+  templateUrl: './listar-despesas.component.html',
+  styleUrl: '../styles/despesas.scss'
 })
 
-export class ListarCompromissosComponent {
-  compromissos?: ListarCompromissosViewModel[];
+export class ListarDespesasComponent {
+  despesas?: ListarDespesasViewModel[];
 
   constructor( private route: ActivatedRoute ) {}
 
   ngOnInit(): void {
-    this.compromissos = this.route.snapshot.data['compromissos'];
+    this.despesas = this.route.snapshot.data['despesas'];
   }
 }
